06813ae26527cf6255626e52963ede74a9c9245a
Sybe Aerts <s1144856@student.hsleiden.nl>
2023-11-18 12:41:55 +0100

    pop up met velden en update button

--- src/app/pop-up-update-product/pop-up-update-product.component.ts
+++ src/app/pop-up-update-product/pop-up-update-product.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-pop-up-update-product',
   templateUrl: './pop-up-update-product.component.html',
   styleUrls: ['./pop-up-update-product.component.scss']
 })
 export class PopUpUpdateProductComponent {
-
+  public updateProduct(){
+    console.log("werkt")
+  }
 }
