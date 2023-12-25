036a063b4b5c7e9f248564b9005c30c6a949999e
Sybe Aerts <s1144856@student.hsleiden.nl>
2023-12-25 13:10:11 +0100

    FEAT: pijltje terug in product

--- src/app/view/product/product.component.ts
+++ src/app/view/product/product.component.ts
@@ -54,68 +54,6 @@ export class ProductComponent implements OnInit{
         alert(error.message);
       })
   }
-
-  // public minProduct(): void {
-  //   if (this.amountProduct > 0){
-  //     this.amountProduct -= 1;
-  //   }
-  // }
-  //
-  // public maxProduct(): void {
-  //   this.isProductOutOfStock = this.outOfStock();
-  //   const shoppingCartString = localStorage.getItem("shoppingCart");
-  //   if (shoppingCartString){
-  //     this.shoppingCartItems = JSON.parse(shoppingCartString);
-  //   }
-  //   for (let i = 0; i < this.shoppingCartItems.length; i++){
-  //     if (this.shoppingCartItems[i].id == this.products[0].id){
-  //       this.amountShoppingCartNow = this.shoppingCartItems[i].amount;
-  //     }
-  //   }
-  //
-  //   if (this.products[0].stock != this.amountProduct){
-  //     if (this.amountProduct + this.amountShoppingCartNow + 1 <= this.products[0].stock)
-  //     this.amountProduct += 1;
-  //   }
-  // }
-  //
-  // public addAmount(): void {
-  //   const productId = this.route.snapshot.paramMap.get('id');
-  //   if (this.amountProduct != 0 && !this.isLoginOrLogout) {
-  //     if (localStorage.getItem("shoppingCart") == null) {
-  //       const obj = [{
-  //         id: productId,
-  //         amount: this.amountProduct
-  //       }];
-  //
-  //       localStorage.setItem("shoppingCart", JSON.stringify(obj));
-  //     } else if (localStorage.getItem("shoppingCart") != null) {
-  //
-  //       const currentCartValue = JSON.parse(localStorage.getItem("shoppingCart") ?? "[]");
-  //
-  //       const existingProduct = currentCartValue.find((item: { id: string | null; }) => item.id === productId);
-  //
-  //       if (existingProduct) {
-  //         existingProduct.amount += this.amountProduct;
-  //       } else {
-  //         currentCartValue.push({
-  //           id: productId,
-  //           amount: this.amountProduct
-  //         });
-  //       }
-  //       localStorage.setItem("shoppingCart", JSON.stringify(currentCartValue));
-  //     }
-  //     this.dialog.open(ItemAddedToShoppingCartComponent);
-  //     this.amountProduct = 0;
-  //   }
-  //   else if (this.amountProduct != 0 && this.isLoginOrLogout){
-  //     this.dialog.open(LoginToDoActionComponent);
-  //   }
-  // }
-  //
-  // public outOfStock():boolean{
-  //   return this.products[0].stock == 0;
-  // }
 }
 
 
