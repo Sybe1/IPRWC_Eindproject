4915552d54ae7f080099779afa7a279f2372ce59
Sybe Aerts <s1144856@student.hsleiden.nl>
2023-12-29 13:59:52 +0100

    FEAT: CRUD voor target audience

--- src/app/view/admin/target-audience-section/pop-up-update-target-audience/pop-up-update-target-audience.component.ts
+++ src/app/view/admin/target-audience-section/pop-up-update-target-audience/pop-up-update-target-audience.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Inject, Output} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {FormBuilder} from "@angular/forms";
+import {ClothingTypeService} from "../../../../services/clothing-type.service";
+import {ClothingType} from "../../../../models/clothing-type";
+import {TargetAudienceService} from "../../../../services/target-audience.service";
+import {TargetAudience} from "../../../../models/target-audience";
 
 @Component({
   selector: 'app-pop-up-update-target-audience',
@@ -6,5 +12,42 @@ import { Component } from '@angular/core';
   styleUrl: './pop-up-update-target-audience.component.scss'
 })
 export class PopUpUpdateTargetAudienceComponent {
+  @Output() onClose: EventEmitter<void> = new EventEmitter<void>();
 
+  myform = this.buildr.group({
+    audience: '',
+    description: ''
+  });
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any, private ref:MatDialogRef<PopUpUpdateTargetAudienceComponent>,
+              private buildr:FormBuilder, private targetAudienceService: TargetAudienceService) {
+  }
+  ngOnInit(): void {
+    this.updateClothingType(this.data.code);
+  }
+  public closePopup(): void{
+    this.ref.close('Closed using function');
+    this.onClose.emit();
+  }
+
+  public updateClothingType(code:any): void{
+    this.targetAudienceService.getTargetAudienceById(code).subscribe(item=>{
+      this.myform.setValue({
+        audience:item.audience,
+        description: item.description
+      })
+    })
+  }
+
+  public saveTargetAudience(): void {
+    const targetAudienceData: TargetAudience = {
+      id: this.data.code || '',
+      audience: this.myform.value.audience || '',
+      description: this.myform.value.description || '',
+    };
+    this.targetAudienceService.updateTargetAudience(targetAudienceData).subscribe(res => {
+      this.closePopup();
+      console.log(res)
+    });
+  }
 }
--- src/app/view/admin/target-audience-section/target-audience-section.component.ts
+++ src/app/view/admin/target-audience-section/target-audience-section.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import {ClothingType} from "../../../models/clothing-type";
+import {ClothingTypeService} from "../../../services/clothing-type.service";
+import {MatDialog} from "@angular/material/dialog";
+import {
+  PopUpUpdateClothingTypeComponent
+} from "../clothing-type-section/pop-up-update-clothing-type/pop-up-update-clothing-type.component";
+import {TargetAudience} from "../../../models/target-audience";
+import {TargetAudienceService} from "../../../services/target-audience.service";
+import {
+  PopUpUpdateTargetAudienceComponent
+} from "./pop-up-update-target-audience/pop-up-update-target-audience.component";
 
 @Component({
   selector: 'app-target-audience-section',
@@ -6,5 +17,45 @@ import { Component } from '@angular/core';
   styleUrl: './target-audience-section.component.scss'
 })
 export class TargetAudienceSectionComponent {
+  targetAudiences: TargetAudience[] = [];
 
+  constructor(private targetAudienceService: TargetAudienceService, private dialog: MatDialog) {
+  }
+
+  ngOnInit() {
+    this.getTargetAudiences()
+  }
+
+  public getTargetAudiences(): void{
+    this.targetAudienceService.getTargetAudiences().subscribe((response: any[]) => {
+      this.targetAudiences = response
+    })
+  }
+
+  deleteTargetAudience(targetAudienceId: string) {
+    if(confirm('Are you sure you want to delete this clothing type?')) {
+      this.targetAudienceService.deleteTargetAudience(targetAudienceId).subscribe(
+        (resp) => {
+          console.log(resp)
+          this.getTargetAudiences();
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  public openPopup(code:any, title :any): void{
+    const dialogRef = this.dialog.open(PopUpUpdateTargetAudienceComponent,{
+      width:'18%',
+      data: {
+        title: title,
+        code: code
+      }
+    });
+    dialogRef.componentInstance.onClose.subscribe(() => {
+      this.getTargetAudiences();
+    });
+  }
 }
