d97a682329b76d7c7aa9d66a3ad4dce140b53ff7
Sybe Aerts <s1144856@student.hsleiden.nl>
2023-12-30 16:32:19 +0100

    FEAT: toegevoegd dat je voor clothingtype/targetaudience geen jwt nodig hebt

--- src/app/conf/customer-interceptor.interceptor.ts
+++ src/app/conf/customer-interceptor.interceptor.ts
@@ -11,10 +11,12 @@ export class CustomerInterceptorInterceptor implements HttpInterceptor {
     const isAuthenticationRequest = request.url.endsWith('/auth/authenticate');
     const isRegister = request.url.endsWith('/auth/register');
     const getProduct = request.url.endsWith('/product/all');
+    const getAllClothingTypes = request.url.endsWith('/clothingType/all');
+    const getAllTargetAudiences = request.url.endsWith('/targetAudience/all');
     const getAllUsers = request.url.endsWith('/user/all');
     const getProductById = request.url.startsWith('http://localhost:8080/product/find');
 
-    if (!isAuthenticationRequest && !getProduct && !getProductById && !isRegister && !getAllUsers) {
+    if (!isAuthenticationRequest && !getProduct && !getProductById && !isRegister && !getAllUsers && !getAllClothingTypes && !getAllTargetAudiences) {
       const token = localStorage.getItem('loginToken');
       const newCloneRequest = request.clone({
         setHeaders: {
