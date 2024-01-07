90bcb1cb4a9c51b90f2fc3d6374ec250da34b356
Sybe Aerts <s1144856@student.hsleiden.nl>
2024-01-07 21:09:02 +0100

    FEAT: opstart/shutdown files

---
+++ shutdown.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kill $(cat ./angular_pid.file)
---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /pad/naar/je/angular/app
+
+nohup ng serve --host 157.245.69.213 > log.txt 2>&1 &
+
+echo $! > ./angular_pid.file
